5b7deb396a149fc33a8635ace8748e9ca7417b14
edfus <edfusedfus@gmail.com>
2020-12-24 00:08:44 +0800

    Separated and fortified the builder module

--- development/watcher.js
+++ development/watcher.js
@@ -1,86 +1,9 @@
-const fs = require('fs');
 const chokidar = require('chokidar');
-const path = require('path');
 const notify = require('./notifications');
-const jscompiler = require('./jscompiler');
-const postcssCompiler = require('./postcss');
-const terser = require('terser');
-
-let config = {
-    js: {
-        src: 'src/js',
-        dest: 'dist/js',
-    },
-    css: {
-        src: 'src/postcss',
-        dest: 'dist/css',
-    }
-};
+const { buildGlightboxJS, buildGlightboxCSS } = require("./builder");
 
 /**
- * Handle Javascript files
- * compile the javascript files
- * to es2015, minify and sync the files
- *
- * @param {string} file path
- */
-async function handleJavascript(file) {
-    file = path.join(config.js.src, 'glightbox.js');
-
-    const name = path.basename(file);
-
-    const res = await jscompiler({
-        file,
-        dest: config.js.dest,
-        format: 'umd',
-        sourcemap: false,
-        moduleID: 'GLightbox'
-    }).catch(error => console.log(error));
-
-    if (!res) {
-        notify('Build Error', `View logs for more info`);
-        console.log(res)
-        return false;
-    }
-
-    const minName = name.replace('.js', '.min.js');
-    const processed = path.join(config.js.dest, name);
-    const code = fs.readFileSync(processed, 'utf8');
-    const minified = terser.minify(code);
-    const minifyPath = path.join(config.js.dest, minName);
-    fs.writeFileSync(minifyPath, minified.code);
-
-    notify('Javascript Build', `Compiled and Minified ${name}`);
-}
-
-
-/**
- * Handle Postcss files
- * compile the css files
- *
- * @param {string} file path
- */
-async function handlePostCSS(file) {
-    const name = path.basename(file);
-    const dest = config.css.dest;
-
-    let res = await postcssCompiler({
-        file,
-        dest,
-        minify: true
-    }).catch(error => console.log(error));
-    if (!res) {
-        return false;
-    }
-    notify('PostCSS Build', `Compiled and Minified ${name}`);
-}
-
-
-
-/**
- * Watcher
- * what the files for the backedn
- * this includes js and css files
+ * Watcher for js and css files
  */
 function filesWatcher() {
     const watcher = chokidar.watch(['src'], {
@@ -93,14 +16,15 @@ function filesWatcher() {
         },
     });
 
-    watcher.on('change', path => {
-        if (path.endsWith('.js')) {
-           return handleJavascript(path);
+    watcher.on('change', async file => {
+        if (file.endsWith('glightbox.js')) {
+           return buildGlightboxJS();
         }
-        if (path.endsWith('.css')) {
-            return handlePostCSS(path);
+        if (file.endsWith('glightbox.css')) {
+            return buildGlightboxCSS();
         }
     })
     watcher.on('ready', () => notify('Watching files', 'Initial scan complete. Ready for changes'))
 }
+
 filesWatcher();
