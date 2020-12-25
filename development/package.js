0c0c2613151da5b414413721e258bf8b729b3943
edfus <edfusedfus@gmail.com>
2020-12-25 12:47:56 +0800

    Rewrite UpdateFileVersion with rw-stream to make it faster:alien:

--- development/package.js
+++ development/package.js
@@ -2,6 +2,9 @@ import fs, { promises as fsp } from 'fs';
 import path from 'path';
 import notify from './notifications.js';
 import __dirname from "./__dirname.js";
+import rw from "rw-stream";
+import { Transform } from "stream";
+import { StringDecoder } from 'string_decoder';
 
 const args = process.argv.slice(2);
 const root_directory = path.join(__dirname, '/..');
@@ -125,20 +128,63 @@ async function createZip(root_directory) {
 
 
 async function updateFileVersion(data) {
-    return fsp.readFile(data.file, { encoding: "utf8" })
-            .then(content => {
-                let regexp = new RegExp(data.search);
-                let matches = null;
+    // set the global flag to ensure search pattern is "stateful"
+    const pattern = new RegExp(data.search, 'g'); 
+    let matches = null;
 
-                while ((matches = regexp.exec(content)) !== null) {
-                    let foundLine = matches[0];
-                    let newLine = foundLine.replace(matches[1], data.replace)
-                    content = content.replace(foundLine, newLine);
-                }
+    return rw_stream(data.file, /\r?\n/, (line, EOF) => {
+        while ((matches = pattern.exec(line)) !== null) {
+            let foundLine = matches[0];
+            let newLine = foundLine.replace(matches[1], data.replace)
+            line = line.replace(foundLine, newLine);
+        }
 
-                return content;
-            })
-            .then(processed => fsp.writeFile(data.file, processed));
+        return EOF ? line : line.concat("\n"); // LF
+    });
+}
+
+async function rw_stream (filepath, separator, callback) {
+    const { readStream, writeStream } = await rw(filepath);
+    
+    return new Promise((resolve, reject) => {
+        let buffer = '';
+        const decoder = new StringDecoder('utf8');
+        
+        readStream
+            .pipe(
+                new Transform({
+                    transform (chunk, encoding, cb) {
+                        chunk = decoder.write(chunk);
+
+                        const parts = chunk.split(separator);
+                        buffer = buffer.concat(parts[0]);
+
+                        if(parts.length === 1) {
+                            return cb();
+                        }
+
+                        // length > 1
+                        this.push(callback(buffer, false));
+
+                        for(let i = 1; i < parts.length - 1; i++) {
+                            this.push(callback(parts[i], false));
+                        }
+
+                        buffer = parts[parts.length - 1];
+                        return cb();
+                    },
+                    flush (cb) { // outro
+                        return cb(
+                                null,
+                                callback(buffer, true)
+                            )
+                    }
+                })
+            )
+            .pipe(writeStream)
+                .on("finish", resolve)
+                .on("error", reject)
+    })
 }
 
 function extractArg(matchPattern) {
